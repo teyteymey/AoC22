369c1d7ccdef9b4684d5964114e1bad5ec3c46f3
teyteymey <tatiana.meyer.u@gmail.com>
2022-12-07 14:54:55 +0100

    day 3 part 2

---
+++ day3/day3.2.py
@@ -0,0 +1,18 @@
+f = open('input.txt', 'r')
+
+glob_points = 0
+lines = f.readlines()
+
+for i in range(0, len(lines), 3) :
+
+    for letter in lines[i]:
+        if (letter in lines[i+1]) and (letter in lines[i+2]):
+            print(letter)
+            if ord(letter) > 96:
+                glob_points += ord(letter)-ord('a')+1
+            else:
+                glob_points += ord(letter)-ord('A')+1+26
+            break
+
+print(glob_points)
+
