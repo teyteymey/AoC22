f0fb4580e05dfb365dfa178810f9e3096fb7d85e
teyteymey <tatiana.meyer.u@gmail.com>
2022-12-13 12:06:43 +0100

    day 4 and setup of 5

--- day3/day3.2.py
+++ day3/day3.2.py
@@ -7,7 +7,6 @@ for i in range(0, len(lines), 3) :
 
     for letter in lines[i]:
         if (letter in lines[i+1]) and (letter in lines[i+2]):
-            print(letter)
             if ord(letter) > 96:
                 glob_points += ord(letter)-ord('a')+1
             else:
---
+++ day4/day4.py
@@ -0,0 +1,24 @@
+f = open('input.txt', 'r')
+
+glob_points = 0
+lines = f.readlines()
+
+for line in lines:
+    line_list = list(line)
+    val = []
+    acu = ''
+    for i in range(0, len(line_list), 1):
+        if line_list[i] == '-' or line_list[i] == ',' or line_list[i] == '\n':
+            val.append(acu)
+            acu = ''
+        else:
+            acu += line_list[i]
+
+    val = [int(i) for i in val]
+
+    if val[0] in range(val[2], val[3]+1) and val[1] in range(val[2], val[3]+1):
+        glob_points += 1
+    elif val[2] in range(val[0], val[1]+1) and val[3] in range(val[0], val[1]+1):
+        glob_points += 1
+
+print(glob_points)
