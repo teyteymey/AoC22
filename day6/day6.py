66839b761f2425cfba781da95631ef24ae4d3b90
teyteymey <tatiana.meyer.u@gmail.com>
2022-12-14 19:27:57 +0100

    day 6

---
+++ day6/day6.py
@@ -0,0 +1,16 @@
+f = open('input.txt', 'r')
+
+line = f.readline()
+line_list = list(line)
+index = -1
+
+for i in range(0, len(line_list), 1):
+    explored_list = line_list[i:i+4]
+    set_list = set(explored_list)
+    if len(set_list) == 4:
+        index = i
+        break
+
+
+print(index+4)
+print(explored_list)
