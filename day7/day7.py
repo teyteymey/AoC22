15c631df60ac724cef0c785a87c1714f3f97a9a5
teyteymey <tatiana.meyer.u@gmail.com>
2022-12-16 21:35:15 +0100

    day 7

--- day7/day7.py
+++ day7/day7.py
@@ -10,30 +10,28 @@ dir_sizes['/'] = 0
 for command in commands:
 
     if command.startswith('$ cd ..'):
-        ndir = "/".join(path_dir)
-        dir_sizes[ndir] += accu_size    #añado al dir donde estaba el tamaño
         path_dir.pop()
-        ndir = "/".join(path_dir)
-        dir_sizes[ndir] += accu_size    #añado al padre el tamaño del hijo
+        #ndir = "/".join(path_dir)
     elif command.startswith('$ cd '):
-        dir_sizes[ndir] += accu_size    #se guarda el tamaño que tenia
         size = len(command)
         nomdir = command[5:size-1]    #nombre dir actual
         path_dir.append(nomdir)       #lo pongo en el path
-        accu_size = 0
         ndir = "/".join(path_dir)
-        dir_sizes[ndir] = accu_size #ini del directorio a 0
+        dir_sizes[ndir] = 0 #ini del directorio a 0
     elif command.startswith('$ ls'):
         continue
     elif command.startswith('dir '):
         continue
     else:   #size of file
         line = command.split(' ')
-        accu_size += int(line[0])
+        #dir_sizes[ndir] += int(line[0])  # se guarda el tamaño que tenia
+        for i in range(1, len(path_dir)+1):
+            parent_path = path_dir[0:i]
+            parent_path = "/".join(parent_path)
+            print(parent_path)
+            dir_sizes[parent_path] += int(line[0])
 
     #print(command, ndir, accu_size, dir_sizes, path_dir)
-
-print("al final", ndir, accu_size, dir_sizes, path_dir)
 
 #at the end to calculate the correct sizes
 for dir in path_dir:
